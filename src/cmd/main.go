afe5263317cca479e09b9250d1c4fc5ac72c1835
Thiago Koster <thiagokoster@gmail.com>
2024-02-20 19:01:59 +0100

    create transaction handler struct

--- src/cmd/main.go
+++ src/cmd/main.go
@@ -1,65 +1,26 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
-	"log"
-	"net/http"
-	"time"
-
 	"github.com/gorilla/mux"
+	"log"
+	"m/internal/transacao"
+	"net/http"
 )
-
-type Transacao struct {
-	Valor     int    `json:"valor"`
-	Tipo      string `json:"tipo"`
-	Descricao string `json:"descricao"`
-}
-
-type RetornoTransacao struct {
-	Limite int `json:"limite"`
-	Saldo  int `json:"saldo"`
-}
-
-type Extrato struct {
-	Total             int         `json:"total"`
-	Saldo             int         `json:"saldo"`
-	DataExtrato       time.Time   `json:"data_extrato""`
-	Limite            int         `json:"limite"`
-	UltimasTransacoes []Transacao `json:"ultimas_transacoes"`
-}
 
 func hello(w http.ResponseWriter, req *http.Request) {
 	fmt.Println("/hello endpoint called")
 	fmt.Fprintf(w, "hello\n")
 }
 
-func clientesTransacoes(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("/clientes/{id}/transacoes endpoint called")
-	vars := mux.Vars(r)
-
-	fmt.Fprintf(w, "hello\n")
-	body, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		panic(err)
-	}
-	log.Println(string(body))
-	var t Transacao
-	err = json.Unmarshal(body, &t)
-	if err != nil {
-		panic(err)
-	}
-	log.Println(t.Valor)
-	log.Println(vars["tipo"])
-}
-
 func main() {
 	r := mux.NewRouter()
-	r.HandleFunc("/clientes/{id}/transacoes", clientesTransacoes)
+	log.Println("Registering routes")
 	r.HandleFunc("/", hello)
+	transacao_handler := transacao.NewHandler()
+	transacao_handler.SetRoutes(r)
 
-	fmt.Println("Server up and listening...")
-	http.Handle("/", r)
-	http.ListenAndServe(":8000", nil)
+	port := 8000
+	log.Println(fmt.Sprintf("Server up and listening on port: %d...", port))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), r))
 }
